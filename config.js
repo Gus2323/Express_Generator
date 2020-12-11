f7f44bd35865a78776c290dbad60b42374238370
Gustavo Castaneda <gustavocastaneda2323@gmail.com>
2020-12-11 14:53:43 -0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
-}
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '702374623742026',
+        clientSecret: '249ea233a92693d787ab1e9a07938139'
+    }
+} 
