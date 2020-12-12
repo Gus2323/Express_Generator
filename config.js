864c85bae39dbbb97e7de885595e58ab203b8661
Gustavo Castaneda <gustavocastaneda2323@gmail.com>
2020-12-12 10:30:17 -0800

    fixed untracked files

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '702374623742026',
-        clientSecret: '249ea233a92693d787ab1e9a07938139'
-    }
-} 
